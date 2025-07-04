e01a3e01fea6fff1e357823f9f9d9039f287d883
Thiago Renan <thiagorenan1108@gmail.com>
2025-07-04 13:04:31 -0300

    Simulação de login no sistema

--- projeto-jsp/src/main/java/servlets/ServletLogin.java
+++ projeto-jsp/src/main/java/servlets/ServletLogin.java
@@ -10,38 +10,56 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
-
-@WebServlet("/ServletLogin")//Mapeamento da URL que vem da tela
+@WebServlet("/ServletLogin") // Mapeamento da URL que vem da tela
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	
-    public ServletLogin() {
-        super();
-     }
 
-	 //Recebe os dados pela URl em pâmetros
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
- 
- 	}
+	public ServletLogin() {
+		super();
+	}
 
-	 //Recebe os dados enviados por um formulário
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
- 
+	// Recebe os dados pela URl em pâmetros
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	// Recebe os dados enviados por um formulário
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
-		
-		if(login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
 
-			ModelLogin modelLogin = new ModelLogin();	
-			modelLogin.setLogin(login); 
-			modelLogin.setSenha(senha); 
-		}else {
+		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+
+			if (modelLogin.getLogin().equalsIgnoreCase("thiago") // Simulando um login
+					&& modelLogin.getSenha().equalsIgnoreCase("1234")) {
+
+				//request.getSession().setAttribute("usuario", modelLogin);//Atributo de sessão / manter o usuário logado no sistema
+
+				RequestDispatcher redirecionar = request.getRequestDispatcher("principal/principal.jsp");
+				redirecionar.forward(request, response);
+
+			} else {
+
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe login e senha corretamente!");
+				redirecionar.forward(request, response);
+			}
+
+		} else {
+
 			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
 			request.setAttribute("msg", "Informe login e senha corretamente!");
-			redirecionar.forward(request, response); 
+			redirecionar.forward(request, response);
+
 		}
-	
+
 	}
 
 }
